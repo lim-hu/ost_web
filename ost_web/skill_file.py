3b4e9ff62844a496a44c78f0cf98d3b1af5bbc4a
limj-hu <milan.lizak@gmail.com>
2023-07-31 22:17:11 +0200

    First upload

---
+++ ost_web/skill_file.py
@@ -0,0 +1,34 @@
+oblivion_skills = {
+    "Blade": "STR",
+    "Blunt": "STR",
+    "Hand To Hand": "STR",
+    "Armorer": "END",
+    "Block": "END",
+    "Heavy Armor": "END",
+    "Athletics": "SPD",
+    "Acrobatics": "SPD",
+    "Light Armor": "SPD",
+    "Security": "AGI",
+    "Sneak": "AGI",
+    "Marksman": "AGI",
+    "Mercantile": "PER",
+    "Speechcraft": "PER",
+    "Illusion": "PER",
+    "Alchemy": "INT",
+    "Conjuration": "INT",
+    "Mysticism": "INT",
+    "Alteration": "WIL",
+    "Destruction": "WIL",
+    "Restoration": "WIL",
+}
+
+oblivion_attributes = {
+    "STR": 0,
+    "END": 0,
+    "SPD": 0,
+    "AGI": 0,
+    "PER": 0,
+    "INT": 0,
+    "WIL": 0
+}
+
